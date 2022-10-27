a61cd504e6d812c02f6a9a31c4de47d0e023b5ed
Hongyu Zhou <hongyu.zhou@compass.com>
2022-10-27 15:51:18 +0800

    bazel demo

---
+++ java-bazel-demo/src/main/java/com/greeting/App.java
@@ -0,0 +1,7 @@
+package com.greeting;
+
+public class App {
+    public static void main(String args[]) {
+        Greeting.sayHiTo("Bazel");
+    }
+}
---
+++ java-bazel-demo/src/main/java/com/greeting/Greeting.java
@@ -0,0 +1,7 @@
+package com.greeting;
+
+public class Greeting {
+    public static void sayHiTo(String name) {
+        System.out.println("Hi, " + name + "!");
+    }
+}
